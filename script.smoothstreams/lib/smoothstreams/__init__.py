702d9cdba9c428f5a8ae30b40e3f17e3a35e0d84
mattitch <36918922+mattitch@users.noreply.github.com>
2018-03-02 15:31:18 +0000

    removed ss

--- script.smoothstreams/lib/smoothstreams/__init__.py
+++
@@ -1,9 +0,0 @@
-import os, sys, inspect
-
-#Include this smoothstreams folder in sys.path so included libs will import properly
-cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
-if cmd_folder not in sys.path:
-    sys.path.insert(0, cmd_folder)
-
-from player import ChannelPlayer #analysis:ignore
-from schedule import Schedule #analysis:ignore
--- script.smoothstreams/ssmain.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from lib import epg
-
-def main():
-    if epg.versionCheck(): return
-    epg.ViewManager()
